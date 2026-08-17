9428eb64e38b13996784025db6094d519575d44e
Akshay Kumar <eng25am0017@dsu.edu.in>
2026-08-17 20:03:25 +0530

    first commit

---
+++ input.java
@@ -0,0 +1,20 @@
+import java.util.*;
+ class input {
+    public static void main(String[]args)
+    {
+        Scanner  sc =new Scanner(System.in);
+        System.out.println("give the 2 number");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        float c=(float)a/b;
+
+        System.out.println((a+b));
+           System.out.println((a*b));
+              System.out.println((a-b));
+                 System.out.println(c);
+                 sc.close();
+
+    }
+
+    
+}
